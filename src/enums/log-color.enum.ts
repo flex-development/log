b296d63e5f931097d392aaa18830c7ea8c4661b4
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-08-21 13:07:54 -0400

    feat(typescript): `enums/LogColor`
    P012-4

---
+++ src/enums/log-color.enum.ts
@@ -0,0 +1,15 @@
+/**
+ * @file Enums - LogColor
+ * @module flog/enums/LogColor
+ */
+
+/**
+ * Log levels mapped to colors.
+ */
+export enum LogColor {
+  DEBUG = 'gray',
+  ERROR = 'red',
+  INFO = 'blue',
+  SUCCESS = 'green',
+  WARN = 'yellow'
+}
