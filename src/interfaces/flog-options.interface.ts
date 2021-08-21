3bef7d333a167e8ae9f4b76e69dab4a6d56d4657
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-08-21 13:59:11 -0400

    feat(typescript): `interfaces/FlogOptions`
    P012-5

---
+++ src/interfaces/flog-options.interface.ts
@@ -0,0 +1,53 @@
+import type { NullishString } from '@flex-development/tutils'
+import type { Level } from '@flog/enums/log-level.enum'
+import type { Color } from 'chalk'
+import figures from 'figures'
+
+/**
+ * @file Interfaces - FlogOptions
+ * @module flog/interfaces/FlogOptions
+ */
+
+/**
+ * `flog` options.
+ */
+export interface FlogOptions {
+  /**
+   * Bold log arguments **and/or** log data.
+   *
+   * @default {args:true,data:false}
+   */
+  bold?: { args?: boolean; data?: boolean }
+
+  /**
+   * Set log data color, and/or override the log figure color set by `level`.
+   *
+   * @default {data:'white'}
+   */
+  color?: { data?: typeof Color; figure?: typeof Color }
+
+  /**
+   * Override the log figure set by `level`, or omit it altogether.
+   */
+  figure?: keyof typeof figures | NullishString
+
+  /**
+   * Log level.
+   *
+   * @default 'DEBUG'
+   */
+  level?: Level
+
+  /**
+   * Use [`echo`][1] instead of `console.log`. Requires [`shelljs`][2].
+   *
+   * [1]: https://github.com/shelljs/shelljs#echooptions-string--string-
+   * [2]: https://github.com/shelljs/shelljs
+   */
+  shell?: boolean
+
+  /**
+   * If `true`, do not log any output.
+   */
+  silent?: boolean
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,5 @@
  * @file Entry Point - Interfaces
  * @module flog/interfaces
  */
+
+export type { FlogOptions } from './flog-options.interface'
