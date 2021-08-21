f7fc9c7f205ee3c98e7e3680e381582ad815875d
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-08-21 13:08:38 -0400

    feat(typescript): `enums/LogLevel`
    P012-4

---
+++ src/enums/log-level.enum.ts
@@ -0,0 +1,15 @@
+/**
+ * @file Enums - LogLevel
+ * @module flog/enums/LogLevel
+ */
+
+/**
+ * Log levels.
+ */
+export enum LogLevel {
+  DEBUG = 'debug',
+  ERROR = 'error',
+  INFO = 'info',
+  SUCCESS = 'success',
+  WARN = 'warning'
+}
