2f0932922e2c62a7e3dedeb2f24c11bcab7b4cc9
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-29 22:52:10 -0400

    chore(tools): cleanup esm loader

--- tools/loaders/esm.mjs
+++ tools/loaders/esm.mjs
@@ -1,45 +1,33 @@
-import fs from 'fs-extra'
-import path from 'path'
-import { getFormat as getFormatTs, resolve as resolveTs } from 'ts-node/esm'
+import path from 'node:path'
 import { createMatchPath, loadConfig } from 'tsconfig-paths'
-import useDualExports from '../helpers/use-dual-exports.mjs'
+/** @type {import('ts-node/dist/esm').registerAndCreateEsmHooks} */
+const hooks = await import('ts-node/esm')
 
 /**
- * @file Helpers - Custom ESM Loader
+ * @file Helpers - Custom ESM Loader Hooks
  * @module tools/loaders/esm
  * @see https://github.com/TypeStrong/ts-node/issues/1007
+ * @see https://nodejs.org/docs/latest-v12.x/api/all.html#esm_hooks
  */
 
 /** @typedef {'builtin'|'commonjs'|'dynamic'|'json'|'module'|'wasm'} Format */
-
-const NODE_MODULES = process.env.NODE_MODULES
-
-// ! Add ESM-compatible export statement to `exports.default` statements
-// ! Fixes: `TypeError: logger is not a function`
-useDualExports([`${NODE_MODULES}/@flex-development/grease/cjs/**`])
+/** @typedef {{ parentURL: string }} ResolveContext */
+/** @typedef {{ url: string }} ResolvedFile */
 
 /**
- * ESM requires all imported files to have extensions. Unfortunately, most `bin`
- * scripts do **not** include extensions.
- *
- * This custom hook provides support for extensionless files by assuming they're
- * all `commonjs` modules.
- *
- * @see https://github.com/nodejs/node/pull/31415
- * @see https://github.com/nodejs/modules/issues/488#issuecomment-589274887
- * @see https://github.com/nodejs/modules/issues/488#issuecomment-804895142
+ * Determines if `url` should be interpreted as a CommonJS or ES module.
  *
  * @async
  * @param {string} url - File URL
- * @param {{}} ctx - Resolver context
- * @param {typeof getFormatTs} defaultGetFormat - Default format function
+ * @param {Record<string, never>} ctx - Resolver context
+ * @param {typeof hooks.getFormat} defaultGetFormat - Default format function
  * @return {Promise<{ format: Format }>} Promise containing module format
  */
 export const getFormat = async (url, ctx, defaultGetFormat) => {
   // Get file extension
   const ext = path.extname(url)
 
-  // Support extensionless files in `bin` scripts
+  // Force extensionless files in `bin` directories to load as commonjs
   if (/^file:\/\/\/.*\/bin\//.test(url) && !ext) return { format: 'commonjs' }
 
   // ! Fixes `TypeError [ERR_INVALID_MODULE_SPECIFIER]: Invalid module
@@ -50,40 +38,53 @@ export const getFormat = async (url, ctx, defaultGetFormat) => {
   // See `tsconfig.json#ts-node.moduleTypes` for file-specific overrides
   if (ext === '.ts') return { format: 'module' }
 
-  // Use default format module for all other files
-  return defaultGetFormat(url, ctx, defaultGetFormat)
+  // Defer to ts-node for all other URLs
+  return await hooks.getFormat(url, ctx, defaultGetFormat)
 }
 
 /**
- * Custom resolver that handles TypeScript path mappings.
+ * Returns the resolved file URL for a given module `specifier` and parent URL.
  *
  * @see https://github.com/TypeStrong/ts-node/discussions/1450
  * @see https://github.com/dividab/tsconfig-paths
  *
  * @async
- * @param {string} specifier - Name of file to resolve
- * @param {{ parentURL: string }} ctx - Resolver context
- * @param {typeof resolveTs} defaultResolve - Default resolver function
- * @return {Promise<{ url: string }>} Promise containing object with file path
+ * @param {string} specifier - Module specifier
+ * @param {ResolveContext} ctx - Function context
+ * @param {string} [ctx.parentURL] - URL of module that imported `specifier`
+ * @param {typeof hooks.resolve} defaultResolve - Default resolve fn
+ * @return {Promise<ResolvedFile>} Resolved file URL
+ * @throws {Error}
  */
 export const resolve = async (specifier, ctx, defaultResolve) => {
-  // Get base URL and path aliases
-  const { absoluteBaseUrl, paths } = loadConfig(process.cwd())
+  // Load TypeScript config to get path mappings
+  const result = loadConfig(process.cwd())
 
-  // Attempt to resolve path based on path aliases
+  // Handle possible error
+  if (result.resultType === 'failed') throw new Error(result.message)
+
+  // Get base URL and path aliases
+  const { absoluteBaseUrl, paths } = result
+
+  // Attempt to resolve specifier using path mappings
   const match = createMatchPath(absoluteBaseUrl, paths)(specifier)
 
   // Update specifier if match was found
-  if (match) {
-    try {
-      const directory = fs.lstatSync(match).isDirectory()
-      specifier = `${match}${directory ? '/index.js' : '.js'}`
-    } catch {
-      specifier = `${match}.js`
-    }
-  }
+  if (match) specifier = match
 
-  return resolveTs(specifier, ctx, defaultResolve)
+  // Defer to ts-node
+  return await hooks.resolve(specifier, ctx, defaultResolve)
 }
 
-export { transformSource } from 'ts-node/esm'
+/**
+ * Applies transformations to `source`.
+ *
+ * @async
+ * @param {Buffer | string} source - Source code to transform
+ * @param {{ format: Format; url: string }} ctx - Function context
+ * @param {Format} [ctx.format] - Module format of source code
+ * @param {string} [ctx.url] - Source code file URL
+ * @param {typeof transformSource} defaultTransformSource - Default transform fn
+ * @return {Promise<{ source: string }>} Promise containing source code
+ */
+export const transformSource = hooks.transformSource
